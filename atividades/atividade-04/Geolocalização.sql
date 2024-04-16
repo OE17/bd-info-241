565813433c07a8ae443e75ef6da60ba95f676b6b
OE17 <138805214+OE17@users.noreply.github.com>
2024-04-16 09:22:58 -0300

    Geolocalização.sql
    Script SQL utilizado para criação de tabelas de acordo com o esquema definido em plantuml.

--- atividades/atividade-04/Geolocalização.sql
+++ atividades/atividade-04/Geolocalização.sql
@@ -1,5 +1,5 @@
 -- Criação da tabela Estado
-CREATE TABLE Estado (
+CREATE TABLE IF NOT EXISTS Estado (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     nome TEXT NOT NULL
 );
@@ -8,7 +8,7 @@ CREATE TABLE Estado (
 INSERT INTO Estado (nome) VALUES ('Ceará');
 
 -- Criação da tabela MesoRegiao
-CREATE TABLE MesoRegiao (
+CREATE TABLE IF NOT EXISTS MesoRegiao (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     nome TEXT NOT NULL,
     estado_id INTEGER NOT NULL,
@@ -19,7 +19,7 @@ CREATE TABLE MesoRegiao (
 INSERT INTO MesoRegiao (nome, estado_id) VALUES ('Jaguaribe', 1);
 
 -- Criação da tabela MicroRegiao
-CREATE TABLE MicroRegiao (
+CREATE TABLE IF NOT EXISTS MicroRegiao (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     nome TEXT NOT NULL,
     mesoregiao_id INTEGER NOT NULL,
@@ -33,7 +33,7 @@ INSERT INTO MicroRegiao (nome, mesoregiao_id) VALUES ('Médio Jaguaribe', 1);
 INSERT INTO MicroRegiao (nome, mesoregiao_id) VALUES ('Serra do Pereiro', 1);
 
 -- Criação da tabela Municipio
-CREATE TABLE Municipio (
+CREATE TABLE IF NOT EXISTS Municipio (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     nome TEXT NOT NULL,
     microregiao_id INTEGER NOT NULL,
@@ -71,7 +71,7 @@ INSERT INTO Municipio (nome, microregiao_id) VALUES ('Pereiro', 4);
 INSERT INTO Municipio (nome, microregiao_id) VALUES ('Potiretama', 4);
 
 -- Criação da tabela IFCampus
-CREATE TABLE IFCampus (
+CREATE TABLE IF NOT EXISTS IFCampus (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     nome TEXT NOT NULL,
     endereco TEXT NOT NULL,
@@ -81,8 +81,20 @@ CREATE TABLE IFCampus (
     FOREIGN KEY(municipio_id) REFERENCES Municipio(id)
 );
 
+-- Inserção de registros na tabela IFCampus
+INSERT INTO IFCampus (nome, endereco, longitude, latitude, municipio_id) 
+VALUES ('IFCE campus Tabuleiro do Norte', 'José Rosendo - Rua Raimundo Noronha Chaves, R. Deoclécio Florêncio Freire, 85, Tabuleiro do Norte - CE, 62960-000', -5.2374150, -38.1452511, (SELECT id FROM Municipio WHERE nome = 'Tabuleiro do Norte'));
+INSERT INTO IFCampus (nome, endereco, longitude, latitude, municipio_id) 
+VALUES ('IFCE Campus Limoeiro do Norte', 'Rua Estevão Remígio de Freitas, 1145 - Monsenhor Otávio, Limoeiro do Norte - CE, 62930-000', -5.1460662, -38.0935475, (SELECT id FROM Municipio WHERE nome = 'Limoeiro do Norte'));
+INSERT INTO IFCampus (nome, endereco, longitude, latitude, municipio_id) 
+VALUES ('IFCE Campus Aracati', 'Rodovia CE-040, Km 137,1 s/n Aeroporto - Conj. Hab. Dr. Abelardo Filho, Aracati - CE, 62800-000', -4.5670353, -37.7913061, (SELECT id FROM Municipio WHERE nome = 'Aracati'));
+INSERT INTO IFCampus (nome, endereco, longitude, latitude, municipio_id) 
+VALUES ('IFCE Campus Jaguaruana', 'Av. Dr. Antônio da Rocha Freitas, 1548-1604 - Jaguaruana, CE, 62823-000', -4.8382799, -37.7851481, (SELECT id FROM Municipio WHERE nome = 'Jaguaruana'));
+INSERT INTO IFCampus (nome, endereco, longitude, latitude, municipio_id) 
+VALUES ('IFCE Campus Morada Nova', 'R. Pref. Raimundo Jose Rabelo, 2717 - Morada Nova, CE, 62940-000', -5.0912437, -38.3649082, (SELECT id FROM Municipio WHERE nome = 'Morada Nova'));
+
 -- Criação da tabela EscolaCampo
-CREATE TABLE EscolaCampo (
+CREATE TABLE IF NOT EXISTS EscolaCampo (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     nome TEXT NOT NULL,
     endereco TEXT NOT NULL,
@@ -93,7 +105,7 @@ CREATE TABLE EscolaCampo (
 );
 
 -- Criação da tabela Assentamento
-CREATE TABLE Assentamento (
+CREATE TABLE IF NOT EXISTS Assentamento (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     nome TEXT NOT NULL,
     endereco TEXT NOT NULL,
